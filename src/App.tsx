ad8a292ae0d9bbb3309e9064225b1fab15a815ff
akshaaatt <tiwariakshat03@gmail.com>
2023-11-24 15:57:16 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,69 @@ function App() {
             width="600"
           />
         )}
+         {activeIframe === 5 && (
+          <iframe
+            src='https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FP_CT_LSW_CHR112.json'
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 6 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FP_CT_LSW_CHR125.json"
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 7 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FP_CT_LSW_CHR126.json"
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 8 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FV_CT_LSW_BNK6401.json"
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 9 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FV_CT_LSW_EIL171.json"
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 10 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FV_CT_LSW_BNK6400.json"
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 11 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FV_CT_LSW_FLM063.json"
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 12 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FV_CT_LSW_EIL160.json"
+            height="600"
+            width="600"
+          />
+        )}
+         {activeIframe === 13 && (
+          <iframe
+          src="https://web2.avataar.me/alhena-js/benchmade/index.html?configLink=https://d38rfgdkhzkrym.cloudfront.net/config/TNT_pr4uZcK34EyjKa/P_SFS-S95-ST_FV_CT_LSW_FLM065.json"
+            height="600"
+            width="600"
+          />
+        )}
       </div>
       <button onClick={toggleIframe}>Toggle Iframe</button>
     </>
